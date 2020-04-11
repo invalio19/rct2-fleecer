b52bf08af8eae24aded56cce15f3113f81270f23
invalio19 <shayslair@hotmail.co.uk>
2020-04-11 12:13:23 +0100

    Added e2e test for ride deletion auto-save bugfix

---
+++ e2e/src/ride-list.e2e-spec.ts
@@ -0,0 +1,39 @@
+import { browser } from 'protractor';
+
+import { RideListPage } from './ride-list.po';
+
+describe('RideList', () => {
+  let page: RideListPage;
+
+  beforeEach(() => {
+    page = new RideListPage();
+  })
+
+  it('should create an instance', () => {
+    expect(page).toBeTruthy();
+  });
+
+  it('should show newly added rides when "delete all rides" is clicked and new rides are added and the page is refreshed', () => {
+    // Arrange
+    page.navigateTo();
+    page.getAddNewAttractionElement().click();
+    page.getRideTypeOptionElement(0, 'Air Powered Vertical Coaster').click();
+    page.getAddNewAttractionElement().click();
+    page.getRideTypeOptionElement(1, 'Bobsleigh Coaster').click();
+
+    // Act
+    page.getDeleteAllRidesElement().click();
+    page.getDeleteAllRidesConfirmElement().click();
+    page.getAddNewAttractionElement().click();
+    page.getRideTypeOptionElement(0, 'Chairlift').click();
+    page.getAddNewAttractionElement().click();
+    page.getRideTypeOptionElement(1, 'Dinghy Slide').click();
+    browser.refresh();
+
+    // Assert
+    page.getRideExpanderElement(0).click();
+    expect(page.getSelectedRideTypeElement(0).getText().then(x => x.trim())).toBe('Chairlift');
+    page.getRideExpanderElement(1).click();
+    expect(page.getSelectedRideTypeElement(1).getText().then(x => x.trim())).toBe('Dinghy Slide');
+  })
+});
---
+++ e2e/src/ride-list.po.ts
@@ -0,0 +1,32 @@
+import { browser, element, by, Key } from 'protractor';
+
+export class RideListPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getAddNewAttractionElement() {
+    return element(by.css('button[data-test-id="add-new-attraction"]'));
+  }
+
+  getRideTypeOptionElement(rideIndex: number, text: string) {
+    return element(by.css(`app-ride[ng-reflect-index="${rideIndex}"] select[data-test-id="ride-type"]`))
+      .element(by.cssContainingText('option', text));
+  }
+
+  getSelectedRideTypeElement(rideIndex: number) {
+    return element(by.css(`app-ride[ng-reflect-index="${rideIndex}"] select[data-test-id="ride-type"] option:checked`));
+  }
+
+  getDeleteAllRidesElement() {
+    return element(by.css('button[data-test-id="delete-all-rides"]'));
+  }
+
+  getDeleteAllRidesConfirmElement() {
+    return element(by.css('button[data-test-id="delete-all-rides-confirm"]'));
+  }
+
+  getRideExpanderElement(rideIndex: number) {
+    return element(by.css(`div[data-test-id="ride-${rideIndex}"] button[data-test-id="expand-ride"]`));
+  }
+}
